2cddb0c248b2f9b202403c023a3c5eb1f37653c7
ThugLife13 <mateuszbaranski6@onet.pl>
2025-03-25 12:22:02 +0100

    new beggining

--- core/lib/imageProcessing.h
+++ core/lib/imageProcessing.h
@@ -1,7 +1,3 @@
-//
-// Created by mateu on 14.01.2025.
-//
-
 #ifndef IMAGEPROCESSING_H
 #define IMAGEPROCESSING_H
 
@@ -25,22 +21,11 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/features2d.hpp>
 
-struct PatternInfo {
-    std::vector<cv::Point> contour;  // punkty konturu
-    cv::Rect boundingBox;            // prostokąt otaczający wzór
-    double area;                     // pole konturu
-};
-
 class imageProcessing {
 public:
     bool procesImages(wxString filePath);
     bool createGrayScale(wxString filePath);
     bool createColorPalette(wxString filePath);
-    std::vector<PatternInfo> ExtractPatterns(wxString filePath);
-    bool drawPatterns(std::vector<PatternInfo> pi, wxString filePath);
-    //bool elementsDetection();
-
-    std::vector<PatternInfo> pi;
 };
 
 
